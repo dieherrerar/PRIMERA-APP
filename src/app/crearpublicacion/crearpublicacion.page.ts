8215929c20079494514968687ba739c6971a6084
die.herrerar <die.herrerar@duocuc.cl>
2024-09-05 14:38:36 -0400

    Se arregló pag de configuración (se le agregaron animaciones. Se cambió el TS de la pagina crearpublicación (Se cambió bastante la lógica para hacerla mas simple)

--- src/app/crearpublicacion/crearpublicacion.page.ts
+++ src/app/crearpublicacion/crearpublicacion.page.ts
@@ -1,45 +1,49 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-crearpublicacion',
   templateUrl: './crearpublicacion.page.html',
   styleUrls: ['./crearpublicacion.page.scss'],
+
 })
 export class CrearpublicacionPage implements OnInit {
-  publication: any = {}; // Define el objeto publication
 
-  constructor() { }
+  titulo:string =""
+  ingredientes :string =""
+  pasos :string =""
 
+  constructor(public mensaje:ToastController, private route:Router,public alerta:AlertController) { }
+
+  async mensajeExito() {
+    const toast = await this.mensaje.create({
+      message: 'Publicación creada con éxito',
+      duration: 2000
+    });
+    toast.present();
+  }
+  async MensajeError() {
+    const toast = await this.mensaje.create({
+      message: 'No deben haber campos vacíos',
+      duration: 2000
+    });
+    toast.present();
+  }
+
+  
+
+  Publicar(){
+    if(this.titulo ==="" && this.ingredientes==="" && this.pasos===""){
+      console.log("No pueden estar los campos vacios ")
+      this.MensajeError()
+    }
+    else{
+      console.log("Publicación creada con éxito")
+      this.mensajeExito()
+      this.route.navigate(["/inicio"])
+    }
+  }
   ngOnInit() {
-  }
-
-  submitPublication() {
-    // Lógica para guardar la publicación en localStorage
-    const publications = JSON.parse(localStorage.getItem('publications') || '[]');
-    publications.push(this.publication);
-    localStorage.setItem('publications', JSON.stringify(publications));
-    console.log('Publicación guardada:', this.publication);
-  }
-
-  onImageChange(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.publication.image = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
-  }
-
-  onVideoChange(event: any) {
-    const file = event.target.files[0];
-    if (file) {
-      const reader = new FileReader();
-      reader.onload = () => {
-        this.publication.video = reader.result as string;
-      };
-      reader.readAsDataURL(file);
-    }
   }
 }
